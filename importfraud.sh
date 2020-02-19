73da4c65f6ae972ca1d4d11bbec37bfea93935f9
calr2112 <61251088+calr2112@users.noreply.github.com>
2020-02-19 16:45:19 -0500

    Delete importfraud.sh

--- importfraud.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/bin/python /home/pindrop/bin/intercept.py import /home/pindrop/scripts/fraud.csv --wave_directory /data/fds/audio/fraud/ --delimiter ',' --datetime_format '%Y-%m-%d %H:%M:%S'
