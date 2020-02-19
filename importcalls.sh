06cd65e56a78dddf217023e3940feb3c61d2f08e
calr2112 <61251088+calr2112@users.noreply.github.com>
2020-02-19 16:45:31 -0500

    Delete importcalls.sh

--- importcalls.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/bin/python /home/pindrop/bin/intercept.py import /home/pindrop/scripts/???.csv --wave_directory /data/fds/audio/NFCU/ --delimiter ',' --datetime_format '%Y-%m-%d %H:%M:%S.%f'
